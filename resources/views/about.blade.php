699db9225531b85c67fe56655ba58590eb7958e8
ryouta <89526043+Zastenchivyy-Smolensky@users.noreply.github.com>
2021-12-15 14:43:44 +0900

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-right">
+                <p><a href="login">ログイン画面へ</a></p>
+                <p><a href="about">概要</a></p>
+                <p><a href="new">新規登録</a></p>
+            </div>
+            <div class="header-left">
+                <a href="index"><h2>制作物アプリ</h2></a>
+            </div>
+        </div>
+    </header>
+
+    <div class="container1">
+        <div class="main">
+            
+            <h1>ようこそ 制作物アプリ</h1>
+        </div>
+    </div>
+
+    <div class="text">
+        <div class="text-container">
+            <h1>制作物アプリ</h1>
+            <p>このアプリはプログラミングを使ってアプリやWebサイトを作った人がポートフォリオとして共有できるようにするためのアプリである            <br><p>このアプリを作ったきっかけはまず第1に情報ネットワークシステムの課題であるということは言うまでもないが<br>
+                アプリを作る授業も機会そしてそれを公開したものを共有するアプリがないためこのアプリを作ったのである.
+            </p>
+        </div>
+    </div>
+
+    <footer>
+        <div class="footer-container">
+            <div class="left">
+                <h1>情報ネットワークシステム</h1>
+            </div>
+            <div class="right">
+                <ul>
+                    <li>情報システム工学科</li>
+                    <li>20c2007</li>
+                    <li>影山亮太</li>
+                </ul>
+            </div>
+            
+        </div>
+    </footer>
+    
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ようこそWebアプリへ</title>
+    <link rel="stylesheet" href="../css/app.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-right">
+                <p><a href="login">ログイン画面へ</a></p>
+                <p><a href="about">概要</a></p>
+                <p><a href="new">新規登録</a></p>
+            </div>
+            <div class="header-left">
+                <h2>制作物アプリ</h2>
+            </div>
+        </div>
+    </header>
+    <div class="container1">
+        <div class="main">
+            <h1>ようこそ 制作物アプリ</h1>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-container">
+            <div class="left">
+                <h1>情報ネットワークシステム</h1>
+            </div>
+            <div class="right">
+                <ul>
+                    <li>情報システム工学科</li>
+                    <li>20c2007</li>
+                    <li>影山亮太</li>
+                </ul>
+            </div>
+            
+        </div>
+    </footer>
+</body>
+</html>
